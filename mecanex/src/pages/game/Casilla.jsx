b8a526f210128d416270519778e8b9875324a9c4
Roberto Hermoso <109271873+robertoHR96@users.noreply.github.com>
2023-07-23 20:33:22 +0200

    Base

---
+++ mecanex/src/pages/game/Casilla.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './Game.css'
+
+export const Casilla = (props) => {
+
+    const { posicion, letraAsignada, inputText } = props;
+
+    const [estadoCasilla, setEstadoCasilla] = useState(0);
+
+    const getEstadoCasilla = () => {
+        let classCasilla;
+        if ((inputText.split('').length < posicion) || (inputText.split('')[posicion] === undefined)) {
+            classCasilla = "-vacia"
+        } else {
+            if (letraAsignada === inputText.split('')[posicion]) {
+                classCasilla = "-valida"
+            } else {
+                console.log(letraAsignada, inputText.split('')[posicion])
+                classCasilla = "-error"
+            }
+        }
+        if (inputText.split('').length === posicion) {
+            classCasilla=classCasilla+" casilla-index"
+        }
+        return classCasilla
+    }
+
+    return (
+        <div className={"estadoCasilla" + getEstadoCasilla()+" casilla"} >{letraAsignada}</div>
+    );
+};
--- mecanex/src/pages/game/Game.jsx
+++ mecanex/src/pages/game/Game.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Casilla } from './Casilla';
+
 
 export const Game = () => {
+
+  const [inputText, setInputText] = useState("")
+
+  const [Texto, setTexto] = useState("Lo más difícil de comenzar a digitar son las tildes, quita mucho tiempo pero toca... Después de eso lo más complicado son los signos de interrogación y exclamación."
+    .split(/(?!^)/)
+    .map(item => (item.trim() === '' ? ' ' : item))
+  );
+
+  useEffect(() => {
+    console.log(Texto)
+  }, [])
+
   return (
-    <div>Game</div>
+    <div className='game'>
+      <div className='centrador'>
+        <div className='centrador logo'>
+          MecaNex
+        </div>
+      </div>
+      <div className='tablero'>
+        {
+          Texto.map((letra, index) => (
+            <Casilla
+              posicion={index}
+              letraAsignada={letra}
+              inputText={inputText}
+            />
+          ))
+        }
+      </div>
+      <div className='centrador'>
+        <input
+          type='text'
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          className='inputText-game'
+        />
+      </div>
+    </div>
   )
 }
+/*
+
+          <Casilla
+            posicion={index}
+            letraAsignada={letra}
+            inputText={inputText}
+          />
+*/
