f9220966a5e64515030f3a9b8f8ea7e0d9f2ccfe
Roberto Hermoso <109271873+robertoHR96@users.noreply.github.com>
2023-10-31 13:00:00 +0100

    docker-compose and DOCKFILE

--- mecanex/src/pages/home/Home.jsx
+++ mecanex/src/pages/home/Home.jsx
@@ -4,7 +4,9 @@ import { useNavigate } from 'react-router-dom';
 export const Home = () => {
     const navigate = useNavigate();
     return (
-        <div>home
+        <div>
+            <div className='titulo-home'>MecanEx</div>
+            <div className='descripcion'>¡Comienza el reto!</div>
             <div onClick={() => navigate("/game")}>Button Game</div>
         </div>
     )
