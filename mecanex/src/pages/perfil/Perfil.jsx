5717a924f0f6e2c301fa3e590e6268a68a003710
Roberto Hermoso <109271873+robertoHR96@users.noreply.github.com>
2024-02-22 21:40:31 +0100

    feat(page-perfil): Creado diseño básico de pantalla perfil usuario

--- mecanex/src/pages/perfil/Perfil.jsx
+++ mecanex/src/pages/perfil/Perfil.jsx
@@ -1,7 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useUsuarioContext } from "../../context/UsuarioContext";
 import axios from "axios";
-
+import imgPerfil from "../../assets/blank-profile-picture.png";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Input,
+  FormFeedback,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Tooltip,
+  Progress,
+} from "reactstrap";
 export const Perfil = (props) => {
   const { user, loginUser, logoutUser } = useUsuarioContext();
 
@@ -13,10 +27,113 @@ export const Perfil = (props) => {
     email: "",
     fotPerfil: "",
   });
+  const [datosPerfilValid, setDatosPerfilValid] = useState({
+    id: false,
+    username: false,
+    first_name: false,
+    last_name: false,
+    email: false,
+    fotPerfil: false,
+  });
 
   useEffect(() => {
-
+    setDatosPerfil({ ...user });
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <div className="perfil">
+        <div className="content-miperfil">
+          <div className="elemento-info-jugador">
+            <div className="title-elemento-info-jugador">
+              <div className="">
+                {user.fotPerfil === null ? (
+                  <img src={imgPerfil} className="fot-perfil" />
+                ) : (
+                  <div>no hola </div>
+                )}
+              </div>
+            </div>
+            <div className="data-perfil">
+							{/*
+              <div className="">
+                <p>
+                  <b>ID:</b>
+                  {" " + user.id}
+                </p>
+              </div>
+							*/}
+              <div className="form-data-perfil">
+                <div className="input-form-data-perfil">
+                  <label>Username</label>
+                  <Input
+                    type="text"
+                    value={datosPerfil.username}
+                    invalid={datosPerfilValid.username}
+                    onChange={(e) =>
+                      setDatosPerfil({
+                        ...datosPerfil,
+                        username: e.target.value,
+                      })
+                    }
+                  />
+                  <FormFeedback>Este email ya esta en uso</FormFeedback>
+                </div>
+                <div className="input-form-data-perfil">
+                  <label>Nombre</label>
+                  <Input
+                    type="text"
+                    value={datosPerfil.first_name}
+                    invalid={datosPerfilValid.first_name}
+                    onChange={(e) =>
+                      setDatosPerfil({
+                        ...datosPerfil,
+                        first_name: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <FormFeedback>Este email ya esta en uso</FormFeedback>
+                <div className="input-form-data-perfil">
+                  <label>Apellidos</label>
+                  <Input
+                    type="email"
+                    value={datosPerfil.last_name}
+                    invalid={datosPerfilValid.last_name}
+                    onChange={(e) =>
+                      setDatosPerfil({
+                        ...datosPerfil,
+                        last_name: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <FormFeedback>Este email ya esta en uso</FormFeedback>
+                <div className="input-form-data-perfil">
+                  <label>email</label>
+                  <Input
+                    type="email"
+                    value={datosPerfil.email}
+                    invalid={datosPerfilValid.email}
+                    onChange={(e) =>
+                      setDatosPerfil({ ...datosPerfil, email: e.target.value })
+                    }
+                  />
+                  <FormFeedback>Este email ya esta en uso</FormFeedback>
+                </div>
+                <div className="centrador-doble-hor-estric ">
+                  <div className="button button-init centrador button-login">
+                    <b>Cancelar</b>
+                  </div>
+                  <div className="button button-init centrador button-juego">
+                    <b>Aceptar</b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
